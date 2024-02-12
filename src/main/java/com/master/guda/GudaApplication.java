b86f82a60176bda61165e2ee023646f095f337fe
dayywish <davish.cs22@bitsathy.ac.in>
2024-02-12 12:42:39 +0530

    initial commit

---
+++ src/main/java/com/master/guda/GudaApplication.java
@@ -0,0 +1,13 @@
+package com.master.guda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/master/guda/GudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.guda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
